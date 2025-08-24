bfe335ef296be226714f996da2dd0fa92669b836
YuvanX <abhivigneshofficial@gmail.com>
2025-08-24 21:32:15 +0530

    feat: animate logo-ticker

--- components/logo-ticker.tsx
+++ components/logo-ticker.tsx
@@ -1,5 +1,31 @@
+"use client";
+import { motion } from "motion/react";
 export const LogoTicker = () => {
   const images = [
+    {
+      src: "/assets/logo-acme.png",
+      alt: "logo-acme",
+    },
+    {
+      src: "/assets/logo-pulse.png",
+      alt: "logo-pulse",
+    },
+    {
+      src: "/assets/logo-echo.png",
+      alt: "logo-echo",
+    },
+    {
+      src: "/assets/logo-celestial.png",
+      alt: "logo-celestial",
+    },
+    {
+      src: "assets/logo-apex.png",
+      alt: "logo-apex",
+    },
+    {
+      src: "assets/logo-quantum.png",
+      alt: "logo-quantum",
+    },
     {
       src: "/assets/logo-acme.png",
       alt: "logo-acme",
@@ -27,24 +53,24 @@ export const LogoTicker = () => {
   ];
 
   return (
-    <div className="flex items-center justify-center w-full py-20 md:py-28">
+    <div className="flex items-center justify-center w-full py-20 md:py-28 max-w-7xl mx-auto">
       <div className="flex items-center  gap-5">
         <div className="flex-1 md:flex-none text-sm">
           Trusted by top innovative teams
         </div>
-        <div className="flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
-          <div className="flex flex-none gap-14">
+        <div className="flex flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+          <motion.div initial={{ translateX: "-50%" }} animate={{ translateX: "0" }} transition={{ duration: 30, repeat: Infinity, ease: "linear" }} className="flex flex-none gap-14 pr-14 -translate-x-0">
             {images.map((img) => {
               return (
                 <img
                   key={img.alt}
                   src={img.src}
                   alt={img.src}
-                  className="h-6 w-auto"
+                  className="h-6 w-auto flex-none"
                 />
               );
             })}
-          </div>
+          </motion.div>
         </div>
       </div>
     </div>
